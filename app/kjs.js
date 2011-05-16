39b268f104deee1dfc1cc20aaa24cdba14a231a2
Andrew Cholakian <andrew@andrewvc.com>
2011-05-15 20:46:47 -0700

    Improved UI, reorged scripts

--- app/kjs.js
+++ app/kjs.js
@@ -5,25 +5,51 @@ $(function() {
     }
 
     var kjs = window.kjs = {};
-    kjs.initialize = function() {
+    kjs.runBuffer = function() {
+        kjs.code   = kjs.editor.getSelection().doc.$lines.join("\n"); 
+        $('#output').empty();
+        try {
+          kjs.output = eval(kjs.code);
+        } catch (e) {
+          kjs.lastError = e;
+        }
+        console.log(output);     
+    };
+    kjs.initializeEditor = function() {
         kjs.editor = ace.edit("editor");
         kjs.editor.setTheme("ace/theme/twilight");
         
         var JavaScriptMode = require("ace/mode/javascript").Mode;
         kjs.editor.getSession().setMode(new JavaScriptMode());
-
+    };
+    kjs.initializeControls = function() {
         $('#run').click(function(e){
             e.preventDefault();
-             
-            kjs.code   = kjs.editor.getSelection().doc.$lines.join("\n"); 
-            try {
-              kjs.output = eval(kjs.code);
-            } catch (e) {
-              kjs.lastError = e;
-            }
-            console.log(output);
+            kjs.runBuffer();
         });
-        
+
+        var $editor = $('#editor');
+        var shifted = false;
+        $editor.keydown(function(e) {
+            if (e.keyCode == 16) {
+                shifted = true;
+            }
+        });
+        $editor.keyup(function(e) {
+            if (e.keyCode == 16) {
+                shifted = false;
+            }
+        });
+        $editor.keypress(function(e) {
+          if (e.keyCode === 13 && shifted) {
+              e.preventDefault();
+              kjs.runBuffer();
+          }
+        });
+    };
+    kjs.initialize = function() {
+      kjs.initializeEditor();
+      kjs.initializeControls();
     };
     
     kjs.initialize();
